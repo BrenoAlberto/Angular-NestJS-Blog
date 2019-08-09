4ad0ab8b4f7e126136e0af0fabd24cbdfaa649b4
Breno Alberto <brenoaps435@gmail.com>
2019-08-08 22:15:32 -0300

    first commit

---
+++ post-api/src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { BlogModule } from './blog/BlogModule';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    BlogModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres', //blog-user
+      password: 'qaz123456', //password
+      database: 'blog-app',
+      entities: [__dirname + '/**/*Entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
